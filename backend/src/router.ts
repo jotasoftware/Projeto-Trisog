8111b2459c97dc8b1a67f053b886293490bb7d9b
João Pedro <jonhpitter2004@hotmail.com>
2024-08-24 18:58:43 -0300

    feat(backend): first commit and initialize project structure

---
+++ backend/src/router.ts
@@ -0,0 +1,13 @@
+import {Router, Request, Response} from 'express';
+
+const router: Router = Router()
+
+router.get('/', (req: Request, res: Response) => {
+    res.json(
+        {
+            message: 'bom dia'
+        }
+    )
+})
+
+export {router}
